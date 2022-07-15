447c23d2f6cae331e0ca833e1c47c9ab58e7f679
Anne Göbels <annegoebels95@gmail.com>
2022-07-15 17:19:47 +0200

    create basic modellist and viewer

---
+++ static/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+    {
+        name: "Model 1",
+        id: "1",
+    },
+    {
+        name: "Model 2",
+        id: "2",
+    },
+    {
+        name: "Model 3",
+        id: "3",
+    },
+    {
+        name: "Model 4",
+        id: "4",
+    },
+    {
+        name: "Model 5",
+        id: "5",
+    },
+
+]
